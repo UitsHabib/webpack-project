bc363e0b3c41962f3d3fbf56b8cd12b3ecf7564a
bshabib <habibur.rahman@brainstation-23.com>
2022-07-17 10:11:18 +0600

    First commit.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
 var webpack = require('webpack');
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENODR_LIBS = ['react', 'lodash', 'redux', 'react-redux', 'react-dom', 'faker', 'react-input-range', 'redux-form', 'redux-thunk'];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENODR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, 
+      {
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
